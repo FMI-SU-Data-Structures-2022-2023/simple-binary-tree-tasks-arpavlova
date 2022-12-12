ae3c3e12149ccc4db421bbda936db42c5afe7469
arpavlova <115587580+arpavlova@users.noreply.github.com>
2022-12-12 21:57:22 +0200

    Update solution.cpp

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,29 +1,92 @@
 #include "solution.h"
 
-int countElements(Node* tree) {
-    return -1;
+int countElements(Node* tree) 
+{
+    if (!tree) { return 0; }
+
+    return 1 + countElements(tree->left) + countElements(tree->right);
 }
 
-int countLeafs(Node* tree) {
-    return -1;
+int countLeafs(Node* tree) 
+{
+    if (!tree) { return 0; }
+    if (!tree->left && !tree->right) return 1;
+    return countLeafs(tree->left) + countLeafs(tree->right);
 }
 
-int height(Node* tree) {
-    return -1;
+int height(Node* tree) 
+{
+    if (!tree) { return 0; }
+    return (1 + height(tree->left) >= 1 + height(tree->right)) ? 1 + height(tree->left) : 1 + height(tree->right);
 }
 
-int sumElements(Node* tree) {
-    return -1;
+int sumElements(Node* tree) 
+{
+    if (!tree) { return 0; }
+
+    return tree->key + sumElements(tree->left) + sumElements(tree->right);
 }
 
-int sumElementsAtLevel(Node* tree, unsigned level) {
-    return -1;
+int sumElementsAtLevel(Node* tree, unsigned level) 
+{
+    if (!tree) { return 0; }
+    if (!level) { return tree->key; }
+    return sumElementsAtLevel(tree->left, level-1) + sumElementsAtLevel(tree->right, level-1);
+
 }
 
-bool isSymmetric(Node* tree) {
+bool areSubtreesSymmetric(Node* left, Node* right)
+{
+    if (!left && !right) { return true; }
+
+    if (!left || !right) { return false; }
+
+    if (left->key == right->key)
+    {
+        return areSubtreesSymmetric(left->left, left->right) && areSubtreesSymmetric(left->right, left->left);
+    }
+
+
     return false;
 }
 
-bool isBST(Node* tree) {
-    return false;
+bool isSymmetric(Node* tree) 
+{
+    if (!tree) { return true; }
+    return areSubtreesSymmetric(tree->left, tree->right);
+}
+
+bool areSubtreesBST(Node* root)
+{
+    if (!root) { return true; }
+    if(root->left && root->right) 
+    { 
+        if(root->left->key < root->key&& root->key <= root->right->key)
+        {
+            return areSubtreesBST(root->left) && areSubtreesBST(root->right);
+        }
+        return false;
+    }
+    if (root->left)
+    {
+        if (root->left->key < root->key)
+        {
+            return areSubtreesBST(root->left);
+        }
+        return false;
+    }
+    if (root->right) 
+    {
+        if (root->key <= root->right->key)
+        {
+            return areSubtreesBST(root->right);
+        }
+        return false;
+    }
+}
+
+bool isBST(Node* tree)
+{
+    if (!tree) { return true; }
+    return areSubtreesBST(tree);
 }
